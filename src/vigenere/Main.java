2673fc97a47cd6ffc874c2a387f511705aeb96ed
lizethgarcia <lpgarcia.10@gmail.com>
2014-10-28 20:21:03 -0500

    Vigenere
    Cifrado

---
+++ src/vigenere/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vigenere;
+
+/**
+ *
+ * @author Lizz
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Vigenere c;
+        c=new Vigenere("profesional","decimo");
+
+    }
+
+}
